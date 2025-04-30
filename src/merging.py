d83aa9102cfbb32c786d96719ba4b68df4e436c2
Michelle Sanford <mrsanford@wm.edu>
2025-04-29 20:05:32 -0400

    added logging

--- src/merging.py
+++ src/merging.py
@@ -2,6 +2,8 @@ import pandas as pd
 import ast
 from typing import List
 from src.utils.logging import setup_logger
+from src.helpers import TMDB_CRIT_COLS, GENRES_CRIT_COLS, BUDGET_CRIT_COLS
+from src.cleaning import clean_tmdb_to_csv, clean_genres_to_csv, clean_budgets_to_csv
 
 
 # drop the null values first in tmdb, genres, and budgets
@@ -84,7 +86,6 @@ def clean_merged_columns(df: pd.DataFrame) -> pd.DataFrame:
         if col.endswith('_x'):
             base_col = col[:-2]
             corresponding_y = base_col + '_y'
-
             if corresponding_y in new_df.columns:
                 new_df[base_col] = new_df[col].combine_first(new_df[corresponding_y])
                 new_df = new_df.drop(columns=[col, corresponding_y])
@@ -104,10 +105,9 @@ def create_genre_table(df: pd.DataFrame, genre_col: str = 'genre') -> pd.DataFra
     """
     Builds a genre lookup table from all unique genre names in the dataset
     """
-    # Flatten all genres into a single list
+    # flattens all genres into a single list
     all_genres = set(genre for sublist in df[genre_col] for genre in sublist)
-
-    # Create genre table
+    # creates genre table
     genre_table = pd.DataFrame({
         'genre_id': range(1, len(all_genres) + 1),
         'genre_name': sorted(all_genres)
@@ -133,3 +133,48 @@ def create_movie_genre_links(df: pd.DataFrame, genre_table: pd.DataFrame, genre_
         how='left'
     )[['movie_id', 'genre_id']]
     return link_table
+
+# final cleaning and merging steps put together
+def cleaning_and_merging(tmdb_crit_cols: List[str] = TMDB_CRIT_COLS,
+                            genres_crit_cols: List[str] = GENRES_CRIT_COLS,
+                            budgets_crit_cols: List[str] = BUDGET_CRIT_COLS,
+                            export: bool = False) -> pd.DataFrame:
+    """
+    Cleans and merges MoVIZ's datasets in preparation for
+    database ingestion
+    """
+    logger = setup_logger("finalizing_clean","merge_datasets")
+    logger.info('Beginning final clean and merge')
+    # Raw to Processed DataFrames
+    tmdb_df = clean_tmdb_to_csv(export=False)
+    genres_df = clean_genres_to_csv(export=False)
+    budgets_df = clean_budgets_to_csv(export=False)
+    logger.info('Cleaning complete on raw datasets. Raw files loaded')
+    # Dropping Additional Null Values in Processed Critical Columns
+    logger.info("Dropping null values in critical columns")
+    tmdb_df = drop_null_in_valid(tmdb_df, tmdb_crit_cols)
+    genres_df = drop_null_in_valid(genres_df, genres_crit_cols)
+    budgets_df = drop_null_in_valid(budgets_df, budgets_crit_cols)
+    logger.info('Null filtering complete')
+    # Implementing the TMDB-Genres Merge and Cleaning the Columns
+    tmdb_genres_merge = merge_movie_ids(tmdb_df, genres_df)
+    movies_master = clean_merged_columns(tmdb_genres_merge)
+    logger.info(f'Merged movies DataFrame shape: {len(movies_master.shape)}')
+    # Assigning movie_id for Budgets
+    budgets_df = assign_budget_movie_id(budgets_df, tmdb_df, genres_df)
+    logger.info(f'Budget DataFrame cleaned: {budgets_df.shape[0]} rows')
+    # Parsing genre column from string to List[str] format
+    movies_master = fix_genre_strings(movies_master)
+    logger.info('Converted genre strings to lists')
+    # Making the Movies-Genre Pivot Table
+    genre_table = create_genre_table(movies_master)
+    logger.info(f'Genre table constructed with {len(genre_table)} genres')
+    movie_genre_pivot = create_movie_genre_links(movies_master, genre_table)
+    logger.info(f'Movie-genre pivot table creation complete: {movie_genre_pivot.shape[0]} links')
+    # if export == True:
+    #     movies_master.to_csv(TMDB_OUTPUT_PATH, index=False)
+    #     genre_table.to_csv(GENRES_OUTPUT_PATH, index=False)
+    #     budgets_df.to_csv(BUDGET_OUTPUT_PATH, index=False)
+    #     movie_genre_pivot.to_csv(os.path.join(PROCESSED_PATH, "movie_genre_links.csv"), index=False)
+    logger.info('Cleaning and merging stage complete')
+    return movies_master, budgets_df, genre_table, movie_genre_pivot
